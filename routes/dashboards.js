bd7161f7f14becb5c61536aed2e087e914ff64ed
zendrias <zenas@live.com>
2022-03-11 08:57:57 -0500

    remove commented out code

--- routes/dashboards.js
+++ routes/dashboards.js
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import * as dashboardsCtrl from '../controllers/dashboard.js'
-// import { isLoggedIn } from '../middleware/middleware.js'
+
 const router = Router()
 
 router.get('/', dashboardsCtrl.index)
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-// import { User } from '../models/user.js'
+
 const router = Router()
 
 router.get('/', function (req, res) {
